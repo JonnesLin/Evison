2cc8e711f39282339452e2666f61c1ba96e72834
Jones <45222374+JonnesLin@users.noreply.github.com>
2021-06-21 00:06:02 +0800

    Update display.py

--- src/display.py
+++ src/display.py
@@ -2,9 +2,6 @@ import torchvision
 from torchvision.transforms import transforms as T
 import torch
 from PIL import Image
-import torchvision
-from torchvision.transforms import transforms as T
-import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
